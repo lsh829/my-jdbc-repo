ba768b1b69e596240279f185b9c2434b925eac7b
lsh <tjsghksusan@naver.com>
2020-03-31 17:42:22 +0900

    2차-main메소드를 포함하고 있는 클래스로 메인 메뉴화면을 구현

---
+++ src/db/member/MemberApp.java
@@ -0,0 +1,37 @@
+package db.member;
+
+import java.util.Scanner;
+
+public class MemberApp {
+	public static void main(String[] args) {
+		Member mm=new Member();
+		Scanner sc=new Scanner(System.in);
+		int ch;
+
+		System.out.println("회원관리 프로그램");
+		
+		try {
+			while(true) {
+				do {
+					System.out.print("1.등록 2.수정 3.삭제 4.아이디검색 5.이름검색 6.리스트 7.종료 =>");
+					ch=sc.nextInt();
+				} while(ch<1 || ch>7);
+				
+				if(ch==7) {
+					break;
+				}
+				
+				switch(ch) {
+				case 1:mm.insertMember(); break;
+				case 2:mm.updateMember(); break;
+				case 3:mm.deleteMember(); break;
+				case 4:mm.findById(); break;
+				case 5:mm.findByName(); break;
+				case 6:mm.listMember(); break;
+				}
+			}
+		}finally {
+			sc.close();
+		}
+	}
+}
