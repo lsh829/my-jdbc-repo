01d9ea473f0d3000bbe15425415a795110c10708
lsh <tjsghksusan@naver.com>
2020-04-01 17:20:01 +0900

    2차 - PrepareStatement 이용 연관, main 메소드를 포함하고 있는 클래스로 메인화면을 구현

---
+++ src/db/employee2/App.java
@@ -0,0 +1,35 @@
+package db.employee2;
+
+import java.util.Scanner;
+
+import com.util.DBConn;
+
+public class App {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		Employee emp=new Employee();
+		Salary sal=new Salary(emp);
+		
+		System.out.println("인사관리 프로그램");
+		
+		int ch;
+		while(true) {
+			System.out.println("\n[Main]");
+			do {
+				System.out.print("1.사원관리 2.급여관리 3.종료 => ");
+				ch = sc.nextInt();
+			}while(ch<1||ch>3);
+			
+			if(ch==3) break;
+			
+			switch(ch) {
+			case 1:emp.employeeManage();break;
+			case 2:sal.salaryManage();break;
+			}
+		}
+
+		sc.close();
+		DBConn.close(); 
+	}
+}
